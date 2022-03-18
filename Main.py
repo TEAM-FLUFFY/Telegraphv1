4ff9ead354f29f037a5426e8818dc1a82bb3867a
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-03-18 19:25:22 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+
+PyrogramBot = Client(
+    "PyrogramBot",
+    api_hash="89b2d2c0120a5ae9e41b9891fdcc3f8f",
+    api_id="9024035",
+    bot_token="5294643076:AAHKKdWkgP87q435FJNtnfP-r30melj4s0Y",
+    plugins=dict(root="PyrogramBot")
+  )
+
+
+PyrogramBot.run()
+    
